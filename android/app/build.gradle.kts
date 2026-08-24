acea9e001a9166d104bcceb637c7217d02be4191
Yann Villellas <yvillellas@gmail.com>
2026-08-24 16:54:16 +0200

    chore(build): upgrade Glance to 1.3.0-alpha02

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.glance:glance-appwidget:1.2.0-rc01")
+    implementation("androidx.glance:glance-appwidget:1.3.0-alpha02")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.material3:material3")
